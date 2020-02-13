010cf60eaaf04c72922fde3f24f47a8d094be65a
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-02-13 10:00:43 +0100

    Add files via upload

---
+++ janjaninRobertProvjeraZnanjaPrvi/janjaninRobertProvjeraZnanjaPrvi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace janjaninRobertProvjeraZnanjaPrvi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 7;
+            double c = 12.5;
+            double d = 16.7;
+            double y;
+            y = (a + b + c + d) / 4;
+            Console.WriteLine("Prosjek je: "+y);
+            Console.ReadKey();
+        }
+    }
+}
